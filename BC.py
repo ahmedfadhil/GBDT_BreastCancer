c429933edd74189dc6e8674ff21be2ab151e8504
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-10-17 01:19:41 +0200

    applying the model on breast cancer data

--- BC.py
+++ BC.py
@@ -9,7 +9,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 fig, subaxes = plt.subplots(1, 1, figsize=(6, 6))
 clf = GradientBoostingClassifier()
 clf.fit(X_train, y_train)
-title = 'Gradient Boosted Decision Trees'
-plot_class_regions_for_classifier_subplot(clf, X_train, y_train, X_test, y_test, title, subaxes)
-
-plt.show()
+print('Breast cancer dataset (learning rate = 0.1,max depth = 3)')
+print('Accuracy of GBDT classifier on training set{:.2f}'.format(clf.score(X_train, y_train)))
+print('Accuracy of GBDT on testing set'.format(clf.score(X_test, y_test)))
+clf = GradientBoostingClassifier(learning_rate=0.01, max_depth=2, random_state=0)
+clf.fit(X_train,y_train)
+print('Breast cancer dataset (learning rate = 0.1,max depth = 3)')
+print('Accuracy of GBDT classifier on training set{:.2f}'.format(clf.score(X_train, y_train)))
+print('Accuracy of GBDT on testing set'.format(clf.score(X_test, y_test)))
